5f55b8be5c55ae6cf832db156c6867193432745f
ari-trinity <arielle.taylor@atlasschool.com>
2025-06-04 20:49:14 -0500

    used print function to display variables. testing in progress

--- variables_datatypes.py
+++ variables_datatypes.py
@@ -1,6 +1,9 @@
 # Creating variables (these are like labeled boxes!)
-our_integer = 10
-our_float = 20.5
-our_string = "Hello, Python!"
-our_bool = True
+our_integer = 10 #whole number
+our_float = 20.5 #float for decimal numbers
+our_string = "Hello, Python!" #string for text
+our_bool = True #boolean for true/false
 print(our_integer, our_float, our_string, our_bool)
+
+# Let's see what type each variable is!
+print(type(our_integer), type(our_float), type(our_string), type(our_bool))
