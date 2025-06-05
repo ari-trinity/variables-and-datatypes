5ad7ff820541d36399fbb249b8973552a155d0a3
ari-trinity <arielle.taylor@atlasschool.com>
2025-06-04 20:41:01 -0500

    added variables to python file

---
+++ variables_datatypes.py
@@ -0,0 +1,6 @@
+# Creating variables (these are like labeled boxes!)
+our_integer = 10
+our_float = 20.5
+our_string = "Hello, Python!"
+our_bool = True
+print(our_integer, our_float, our_string, our_bool)
